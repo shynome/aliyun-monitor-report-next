a2da73e8e14b695d288040d717d06db12f4f5b9f
shynome <shynome@gmail.com>
2019-08-30 17:15:21 +0800

    limit body

--- pages/api/group/list.api.ts
+++ pages/api/group/list.api.ts
@@ -1,7 +1,7 @@
 import { PageConfig, NextApiRequest, NextApiResponse } from "next";
 import { aliyun } from "~lib/aliyun";
 
-export const config: PageConfig = { api: { bodyParser: { sizeLimit: '1mb' } } }
+export const config: PageConfig = { api: { bodyParser: { sizeLimit: '10k' } } }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   let params = req.body
