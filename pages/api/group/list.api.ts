83c394944965e2ba1d109b425a5d59a68d8b33e3
shynome <shynome@gmail.com>
2019-08-30 17:14:34 +0800

    group list ok

--- pages/api/group/list.api.ts
+++ pages/api/group/list.api.ts
@@ -1,8 +1,10 @@
 import { PageConfig, NextApiRequest, NextApiResponse } from "next";
+import { aliyun } from "~lib/aliyun";
 
 export const config: PageConfig = { api: { bodyParser: { sizeLimit: '1mb' } } }
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  debugger
-  res.end()
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let params = req.body
+  let result = await aliyun.GetGroupList(params)
+  res.json(result)
 }
