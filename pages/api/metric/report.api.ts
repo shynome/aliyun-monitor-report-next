2b38efd1d9e19ea935e97d64270bf8788702990c
shynome <shynome@gmail.com>
2019-09-02 17:54:45 +0800

    add api

--- pages/api/metric/report.api.ts
+++ pages/api/metric/report.api.ts
@@ -1,5 +1,17 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { PageConfig, NextApiRequest, NextApiResponse } from "next";
+import micro, { createError } from "micro";
+import httpStatus from "http-status";
+import { aliyun } from "~lib/aliyun";
+export const config: PageConfig = { api: { bodyParser: { sizeLimit: '1mb' } } }
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default micro(async (req: NextApiRequest, res: NextApiResponse) => {
 
-}
+  let params = req.body
+  let result = await aliyun.GetMetricReport(params).catch(
+    (err: Error) => {
+      throw createError(httpStatus.INTERNAL_SERVER_ERROR, err.message)
+    }
+  )
+  res.json(result)
+
+})
