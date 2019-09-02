e2cf2e4d6e2f840eeed6d2d2830f6d776ade1944
shynome <shynome@gmail.com>
2019-09-02 10:25:06 +0800

    resources ok

--- pages/api/group/resources.api.ts
+++ pages/api/group/resources.api.ts
@@ -1,7 +1,19 @@
 import { PageConfig, NextApiRequest, NextApiResponse } from "next";
+import { aliyun } from "~lib/aliyun";
+import micro, { createError } from 'micro'
+import httpStatus from 'http-status'
 
-export const config: PageConfig = {}
+export const config: PageConfig = { api: { bodyParser: { sizeLimit: '2mb' } } }
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default micro(async (req: NextApiRequest, res: NextApiResponse) => {
 
-}
+  let params = req.body
+  
+  let result = await aliyun.GetGroupResources(params).catch(
+    (err: Error) => {
+      throw createError(httpStatus.INTERNAL_SERVER_ERROR, err.message)
+    }
+  )
+  res.json(result)
+
+})
