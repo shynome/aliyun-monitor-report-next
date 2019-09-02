863b04cc22646546ed55e47687110f699cffbaba
shynome <shynome@gmail.com>
2019-09-02 17:56:22 +0800

    fix

--- pages/index/Report.tsx
+++ pages/index/Report.tsx
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { LinearProgress, Table, TableBody, TableHead, TableRow, TableCell } from "@material-ui/core";
 import { api } from "~pages/api/client";
-
-type ReportResp = {
-  Errors: string
-  Report: { [k: string]: Array<Report> }
-}
+import { MetricReport } from "~modules/aliyun/metric_report";
 
 type ReportParams = {
   GroupId: number
@@ -14,29 +10,13 @@ type ReportParams = {
   Namespaces?: string[]
 }
 
-type Dimension = {
-  DisplayName: string
-  Name: string
-  Error: string
-  Max: number
-  Avg: number
-}
-
-type Report = {
-  Category: string
-  Id: string
-  InstanceId: string
-  InstanceName: string
-  RegionId: string
-  Dimensions: Array<Dimension>
-  Error: string
-}
+export const isEmpty = (val: any) => typeof val === 'undefined' || val === null
 
 export const Report: React.StatelessComponent<ReportParams> = (props) => {
 
   let namespaces = props.Namespaces || []
 
-  let [resp, setResp] = useState<ReportResp>(null as any)
+  let [report, setReport] = useState<MetricReport>(null as any)
   let reqParams: ReportParams = {
     GroupId: props.GroupId,
     StartTime: props.StartTime,
@@ -45,23 +25,19 @@ export const Report: React.StatelessComponent<ReportParams> = (props) => {
   useEffect(() => {
     void async function () {
       console.log(reqParams)
-      let resp = await api.post<ReportResp>('/metric/report', reqParams)
-      setResp(resp.data)
+      let resp = await api.post<MetricReport>('/metric/report', reqParams)
+      setReport(resp.data)
     }()
   }, [JSON.stringify(reqParams)])
 
-  if (resp === null) {
+  if (report === null) {
     return <LinearProgress />
-  }
-
-  if (resp.Errors) {
-    return <div>{resp.Errors}</div>
   }
 
   return (
     <>
-      {Object.keys(resp.Report).filter(v => namespaces.indexOf(v) !== -1).map((key) => {
-        let reports = resp.Report[key]
+      {Object.keys(report).map((key) => {
+        let reports = report[key]
         return (
           <div key={key}>
             <h3>{key}</h3>
@@ -74,12 +50,12 @@ export const Report: React.StatelessComponent<ReportParams> = (props) => {
               <TableBody>
                 {reports.map((report) => {
                   let h = report.Dimensions.length
-                  return report.Dimensions.sort((a,b)=>a.Name>b.Name?-1:1).map((d, i) => (
+                  return report.Dimensions.sort((a, b) => a.Name > b.Name ? -1 : 1).map((d, i) => (
                     <TableRow key={report.Id + d.Name}>
                       {i === 0 && <TableCell rowSpan={h} style={{ width: 180 }}>{report.InstanceName || report.InstanceId}</TableCell>}
                       <TableCell>{d.DisplayName}</TableCell>
-                      <TableCell align="right">{d.Max.toFixed(2)}</TableCell>
-                      <TableCell align="right">{d.Avg.toFixed(2)}</TableCell>
+                      <TableCell align="right">{isEmpty(d.Maximum) ? "无值" : d.Maximum.toFixed(2)}</TableCell>
+                      <TableCell align="right">{isEmpty(d.Average) ? "无值" : d.Average.toFixed(2)}</TableCell>
                     </TableRow>
                   ))
                 })}
