336e3dfb85c4b23ab1df7d137003e9c53442ef82
shynome <shynome@gmail.com>
2019-09-09 16:58:03 +0800

    remove used whitespace

--- pages/api/login/AUTH_PUBLIC_PEM.api.ts
+++ pages/api/login/AUTH_PUBLIC_PEM.api.ts
@@ -5,7 +5,5 @@ import { getEnv } from "~utils/env";
 let AUTH_PUBLIC_PEM = JSON.stringify(getEnv('AUTH_PUBLIC_PEM'))
 
 export default micro((req, res) => {
-  res.end(`
-  define([],${AUTH_PUBLIC_PEM})
-  `)
+  res.end(`define([],${AUTH_PUBLIC_PEM})`)
 })
