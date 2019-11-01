5e0157095b6acd21ac45f74e057d508111715411
shynome <shynome@gmail.com>
2019-11-01 14:30:34 +0800

    no ssr

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,29 +2,24 @@ import React from 'react';
 import App, { Container } from 'next/app';
 import { ThemeProvider } from '@material-ui/styles';
 import { SnackbarProvider } from "notistack";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { CssBaseline, NoSsr } from '@material-ui/core';
 import { theme } from './theme';
 
 class MyApp extends App {
-  componentDidMount() {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-  }
-
+  
   render() {
     const { Component, pageProps, } = this.props;
 
     return (
-      <SnackbarProvider>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </SnackbarProvider>
+      <NoSsr>
+        <SnackbarProvider>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </SnackbarProvider>
+      </NoSsr>
     );
   }
 }
