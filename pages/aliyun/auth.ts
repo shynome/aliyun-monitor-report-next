79e230dbe625a00f90989f88e273d11811043ea0
shynome <shynome@gmail.com>
2019-11-01 14:31:03 +0800

    使用新接口返回公钥

--- pages/aliyun/auth.ts
+++ pages/aliyun/auth.ts
@@ -11,20 +11,19 @@ export const encrypt = (secret: string, pulic_pem: string): string => {
   return encrypted
 }
 
-let AUTH_PUBLIC_PEM = new Promise<string>((rl, rj) => {
-  requirejs(['/api/login/AUTH_PUBLIC_PEM'], (public_pem: string) => rl(public_pem), rj)
-})
+let AUTH_PUBLIC_PEM: Promise<string>
+
+const getAUTH_PUBLIC_PEM = (): Promise<string> => {
+  return AUTH_PUBLIC_PEM || (AUTH_PUBLIC_PEM = fetch('/api/login/AUTH_PUBLIC_PEM').then(r => r.text()))
+}
 
 export const encryptToken = async (key: string, secret: string) => {
-  let auth_public_pem = await AUTH_PUBLIC_PEM
+  let auth_public_pem = await getAUTH_PUBLIC_PEM()
   return encrypt([key, secret].join(','), auth_public_pem)
 }
 
 import { AuthTokenUniqueName } from "~libs/aliyun/constants";
 import api from "../api/client";
-export const setToken = (token: string) => {
-  localStorage.setItem(AuthTokenUniqueName, token)
-}
 
 export const checkToken = async (token: string) => {
   await api.get('/group/list', {
