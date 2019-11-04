5c2cf57062e08a03e175a503c31569208a399146
shynome <shynome@gmail.com>
2019-11-04 11:46:33 +0800

    add useFormFields

--- libs/web-utils/form.ts
+++ libs/web-utils/form.ts
@@ -2,14 +2,30 @@ import { useState } from "react";
 
 export type ChangeHandler = React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
 
-export const useFormField = <T=string>(defaultValue:T):[T,ChangeHandler]=>{
+export const useFormField = <T = string>(defaultValue: T): [T, ChangeHandler] => {
 
-  let [ value, setValue ] = useState(defaultValue)
-  
-  let handleChange:ChangeHandler = (e:any)=>{
+  let [value, setValue] = useState(defaultValue)
+
+  let handleChange: ChangeHandler = (e: any) => {
     setValue(e.target.value)
   }
-  
-  return [ value, handleChange ]
-  
+
+  return [value, handleChange]
+
+}
+
+
+export const useFormFields = <T extends object>(defaultFields: T): [T, (t: keyof T) => ChangeHandler] => {
+
+  let [fields, setFields] = useState(defaultFields)
+
+  let saveFormField = (t: keyof T): ChangeHandler => (e: any) => {
+    setFields({
+      ...fields,
+      [t]: e.target.value
+    })
+  }
+
+  return [fields, saveFormField]
+
 }
