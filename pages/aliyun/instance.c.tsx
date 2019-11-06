f0b9e5e9b4b467c01a12ae8e9a5013f471f8eea8
shynome <shynome@gmail.com>
2019-11-06 16:29:14 +0800

    共用状态

--- pages/aliyun/instance.c.tsx
+++ pages/aliyun/instance.c.tsx
@@ -1,19 +1,19 @@
 import React, { Fragment } from "react";
 
-import { AliyunInstanceContainer, useAliyunInstance, useAliyunInstanceSet } from "./instance";
-import { LinearProgress, Paper } from "@material-ui/core";
+import { AliyunInstanceContainer, useAliyunInstanceSet } from "./instance";
+import { LinearProgress } from "@material-ui/core";
 import { useRouter } from "next/router";
 
 export const AliyunInstanceCheck: React.StatelessComponent = (props) => {
 
-  const { aliyun } = useAliyunInstance()
+  const { aliyun } = AliyunInstanceContainer.useContainer()
   const router = useRouter()
   const { pending } = useAliyunInstanceSet()
 
   if (pending) {
     return <LinearProgress />
   }
-
+  
   if (!aliyun && !router.pathname.startsWith('/aliyun/account')) {
     return <LinearProgress />
   }
